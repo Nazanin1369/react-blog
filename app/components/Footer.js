0e84c9c7fe1bbf4d132ecfca27cc98b1cd20798f
Nazanin Delam <nazanin1427@gmail.com>
2016-05-01 20:23:11 -0700

    Added main components

---
+++ app/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+      <footer className="footer">
+        <div className="container">
+            <p className="text-muted">Copy Right 2016 - Nazanin Delam</p>
+        </div>
+      </footer>
+    )
+}
+export default Footer;
---
+++ app/components/Navbar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class Navbar extends React.Component {
+    render() {
+        return (
+            <nav className="navbar navbar-default navbar-fixed-top my-nav">
+                <div className="container">
+                    <div className="navbar-header">
+                    <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                        <span className="sr-only">Toggle navigation</span>
+                        <span className="icon-bar"></span>
+                        <span className="icon-bar"></span>
+                        <span className="icon-bar"></span>
+                    </button>
+                    <a className="navbar-brand" href="#">Nazanin De's Blog</a>
+                    </div>
+                </div>
+            </nav>
+        )
+    }
+}
+
+export default Navbar;
