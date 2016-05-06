3de24dda8fa1a77e5dc2b48aea24051680a5e9dc
Nazanin Delam <nazanin1427@gmail.com>
2016-05-05 21:35:41 -0700

    Added PostEditor component

---
+++ app/components/Editor/PostEditor.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Editor, EditorState} from 'draft-js';
+import DraftEditor from './DraftEditor';
+
+class PostEditor extends React.Component {
+    render() {
+        return <DraftEditor />;
+    }
+}
+
+export default PostEditor;
--- app/config/routes.js
+++ app/config/routes.js
@@ -4,12 +4,14 @@ import Main from '../components/Main';
 import NoMatch from '../components/NoMatch';
 import Content from '../components/Content';
 import Post from '../components/Posts/Post';
+import PostEditor from '../components/Editor/PostEditor'
 
 export default(
     <Router history={browserHistory}>
         <Route path="/" component={Main}>
             <Route path="/blog" component={Content}/>
             <Route path="/post/:postId" component={Post}/>
+            <Route path="/editPost" component={PostEditor}/>
         </Route>
         <Route path="*" component={NoMatch}/>
     </Router>
